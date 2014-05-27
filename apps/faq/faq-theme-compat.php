874101c5f6a147b32332ce92e70355dc4607dc1c
bignetstudio <bignetstudio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-27 07:58:32 +0000

    Make the plugin more theme supported
    git-svn-id: https://plugins.svn.wordpress.org/well-framework/trunk@921499 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- apps/faq/faq-theme-compat.php
+++ apps/faq/faq-theme-compat.php
@@ -4,7 +4,7 @@
 ----------------------------------------------*/
 add_filter( 'template_include', 'faqwell_template_loader' );
 function faqwell_template_loader( $template ) {
-	$find = array('well-framework.php','page.php','index.php');
+	$find = array();
 	$file = '';
 	$typs = 'faqwell';
 	$tax = 'faqwell_cat';
@@ -19,6 +19,10 @@ function faqwell_template_loader( $template ) {
 		$file   = 'taxonomy-'.$tax.'.php';
 		$find[] = $file;
 	}
+	
+	$find[] = 'well-framework.php';
+	$find[] = 'page.php';
+	$find[] = 'index.php';
 	
 	if($file){ $template = locate_template( $find ); }
 
--- apps/logos/logos-theme-compat.php
+++ apps/logos/logos-theme-compat.php
@@ -4,7 +4,7 @@
 ----------------------------------------------*/
 add_filter( 'template_include', 'logowell_template_loader' );
 function logowell_template_loader( $template ) {
-	$find = array('well-framework.php','page.php','index.php');
+	$find = array();
 	$file = '';
 	$typs = 'logowell';
 	$tax = 'logowell_cat';
@@ -19,6 +19,10 @@ function logowell_template_loader( $template ) {
 		$file   = 'taxonomy-'.$tax.'.php';
 		$find[] = $file;
 	}
+	
+	$find[] = 'well-framework.php';
+	$find[] = 'page.php';
+	$find[] = 'index.php';
 	
 	if($file){ $template = locate_template( $find ); }
 
