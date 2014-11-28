49312f16d5be82fe32e5c61b587a5f3758d09757
bignetstudio <bignetstudio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-28 04:22:33 +0000

    Tagged 1.5.1
    git-svn-id: https://plugins.svn.wordpress.org/well-framework/tags/1.5.1@1034216 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ apps/portfolio/templates/grid.php
@@ -0,0 +1,22 @@
+<?php
+global $wefr_portfolio_template_path;
+$isotope = new acoc_isotope_html(array('column'=>$column, 'margin'=>$column_margin));
+$portfolio_query = new WP_Query( $query );
+?>
+<div class="portwell skin-1">
+	<?php if( $portfolio_query->have_posts()): ?>
+    	<?php if($filter == 'yes'){ $isotope->filter('portwell_cat'); } ?>
+    	<?php $isotope->start(); ?>
+        	<?php while ( $portfolio_query->have_posts() ) : $portfolio_query->the_post(); ?>
+            	<?php $isotope->in_loop_start( $isotope->post_tax_class(get_the_ID(), 'portwell_cat')); ?>
+                	<?php include($wefr_portfolio_template_path->dri().'content-portfolio.php'); ?>
+                <?php $isotope->in_loop_end(); ?>
+            <?php endwhile; ?>
+        <?php $isotope->end(); ?>
+        <div style="clear:both;"></div>
+        <?php wp_reset_postdata(); ?>
+        <?php if($pagination == 'yes'){ echo acoc_paginate($portfolio_query); } ?>
+    <?php else: ?>
+    	<?php _e('No Portfolio found.', 'well_textdomain'); ?>
+    <?php endif; ?>
+</div>
--- readme.txt
+++ readme.txt
@@ -3,8 +3,8 @@ Contributors: bignetstudio
 Donate link: http://example.com/
 Tags: portfolio, gallery, shortcode, faq, people, testimonial, doc
 Requires at least: 3.8
-Tested up to: 3.9.1
-Stable tag: 1.4
+Tested up to: 4.0.1
+Stable tag: 1.5.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
