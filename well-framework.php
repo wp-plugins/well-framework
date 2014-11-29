947fdfe22ae11e6046a597ddfe08760e43122dd1
bignetstudio <bignetstudio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-29 07:04:33 +0000

    Fix a error
    git-svn-id: https://plugins.svn.wordpress.org/well-framework/trunk@1034869 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- well-framework.php
+++ well-framework.php
@@ -3,7 +3,7 @@
 Plugin Name: Well Framework
 Plugin URI: http://themeforest.net/user/bignet
 Description: A wordpress Plugin
-Version: 1.5.1
+Version: 1.5.2
 Author: Bignet
 Author URI: http://themeforest.net/user/bignet/portfolio
 
